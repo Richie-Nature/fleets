4ec1a56e18e75388f0435b8637ffbf084017de48
Imoh Obot <obotimoh20@gmail.com>
2021-12-02 14:51:41 +0100

    setup eureka server

--- pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
+++ pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
@@ -4,6 +4,7 @@ import com.udacity.pricing.domain.price.Price;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -17,10 +18,18 @@ public class PricingService {
     /**
      * Holds {ID: Price} pairings (current implementation allows for 20 vehicles)
      */
-    private static final Map<Long, Price> PRICES = LongStream
-            .range(1, 20)
-            .mapToObj(i -> new Price("USD", randomPrice(), i))
-            .collect(Collectors.toMap(Price::getVehicleId, p -> p));
+    private static final Map<Long, Price> PRICES;
+
+    static {
+        Map<Long, Price> map = new HashMap<>();
+        for (long i = 1; i < 20; i++) {
+            Price p = new Price("USD", randomPrice(), i);
+            if (map.put(p.getVehicleId(), p) != null) {
+                throw new IllegalStateException("Duplicate key");
+            }
+        }
+        PRICES = map;
+    }
 
     /**
      * If a valid vehicle ID, gets the price of the vehicle from the stored array.
